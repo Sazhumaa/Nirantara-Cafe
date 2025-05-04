5255b697a087125b0c89a81bf18e3f21cb618fbd
Satriyo Agung <sazhumaakun@gmail.com>
2025-05-04 21:46:05 +0700

    No logic Buying Pages

---
+++ src/Buying Page.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('click', () => {
+      document.querySelectorAll('.nav-item').forEach(nav => {
+        nav.classList.remove('active');
+      });
+      item.classList.add('active');
+    });
+  });
